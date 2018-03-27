c49c7d8e190913bdff95c1193a7365be36060bd1
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-03-27 08:54:50 -0400

    backend

--- Lab 4.3 Button Games/src/BackEnd.java
+++ Lab 4.3 Button Games/src/BackEnd.java
@@ -1,4 +1,53 @@
+public class BackEnd 
+	{
 
-public class BackEnd {
+		private int count;
+		private boolean buttonPressed;
+		
+		public boolean isButtonPressed() 
+		{
+			return buttonPressed;
+		}
 
+		public int getCount()
+		{
+			return count;
+		}
+
+		public BackEnd(int x)
+		{ 
+			this.count= count;
+		;
+		 this.buttonPressed= buttonPressed;
+		 	count=x;
+		 	updateCount(x);
+		 	editFile("testcsv.txt",x);	
+		}
+	    
+		
+		private static File editFile (String filename,int count)
+		{
+			Path pathtoFile=Paths.get(filename);
+		 try(BufferedWriter bw=Files.newBufferedWriter(pathtoFile, StandardCharsets.US_ASCII))
+		   { 
+			 Integer countstring= new Integer(count);
+			 bw.write(countstring.toString()+",");
+		   }
+		}
+		
+		public int updateCount(int count)
+		{
+			if (buttonPressed)
+			{
+				count++;
+			}
+				else
+			{
+			count = count+0 ;
+			}
+		}	
+			
+		
+
+	}
 }
